9eb4ef10148554ed2da2529fe6c8f66bd5168af5
유윤지 <105447233+llcodingll@users.noreply.github.com>
2024-01-29 11:28:34 +0900

    Update 1342-number-of-steps-to-reduce-a-number-to-zero.java

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -3,6 +3,23 @@ class Solution {
         int steps = 0;
     
         while (num > 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num--;
+            }
+            steps++;
+        }
+        return steps;
+    }
+}
+
+
+class Solution {
+    public int numberOfSteps(int num) {
+        int steps = 0;
+    
+        while (num > 0) {
             if ((num & 1) == 0) {
                 num >>= 1;
             } else {
